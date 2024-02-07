dddf40a6a4e8e27d093113e6f53de6ee74f67921
ssergioz5 <51833963+ssergioz5@users.noreply.github.com>
2024-02-07 17:57:12 +0100

    Update app.js

--- _js/app.js
+++ _js/app.js
@@ -7,6 +7,12 @@ $(document).ready(function() {
     window.scrollTo(0, 0);
   });
 
+  (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; typeof namespace === "string" ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar); return; } p(cal, ar); }; })(window, "https://cal.romerosland.com/embed/embed.js", "init");
+Cal("init", "cv", {origin:"https://cal.romerosland.com"});
+
+  Cal.ns.cv("floatingButton", {"calLink":"sromero/cv","calOrigin":"https://cal.romerosland.com","buttonText":"Agendar reunion"});
+  Cal.ns.cv("ui", {"styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":false,"layout":"month_view"});
+  
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS.load('landing', 'assets/particles.json', function() {});
 
