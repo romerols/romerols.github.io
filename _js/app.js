8763fd71ac1baded6b9ccfde03c426f3725478d4
ssergioz5 <51833963+ssergioz5@users.noreply.github.com>
2024-02-07 18:15:47 +0100

    Update app.js

--- _js/app.js
+++ _js/app.js
@@ -34,14 +34,7 @@ $(document).ready(function() {
     once: true
   });
 
-  randomizeOrder();
-
-  (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; typeof namespace === "string" ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar); return; } p(cal, ar); }; })(window, "https://cal.romerosland.com/embed/embed.js", "init");
-Cal("init", "cv", {origin:"https://cal.romerosland.com"});
-
-  Cal.ns.cv("floatingButton", {"calLink":"sromero/cv","calOrigin":"https://cal.romerosland.com","buttonText":"Agendar reunion"});
-  Cal.ns.cv("ui", {"styles":{"branding":{"brandColor":"#000000"}},"hideEventTypeDetails":false,"layout":"month_view"});
-  
+  randomizeOrder();  
 });
 
 /* FUNCTIONS */
